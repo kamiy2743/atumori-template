9abc17c2b22a8f62d9fc3b9f9a1583afb99569bf
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2020-05-24 09:39:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ $(function() {
    $(".clicked-y").append('<img src="mile-fur/circle.png" class="icon-mark circle y">');
    $(".clicked-n").append('<img src="mile-fur/cross.png" class="icon-mark cross n">');
    $(".clicked-g").append('<img src="mile-fur/heart.png" class="icon-mark heart g">');
-
   });
 
   $("#friend-cord").click(function() {
@@ -38,7 +37,7 @@ $(function() {
     html2canvas(document.querySelector("#capture")).then(canvas => {
     document.body.appendChild(canvas)
     $("canvas").attr("id","target");
-    $("canvas").remove();
+    $("canvas").addClass("none");
     $(".capture-btn-1").removeClass("not-save");
     $("#alert").addClass("none");
     $("#capture").addClass("none");
@@ -51,10 +50,16 @@ $(function() {
     } else {
       $("#sucsess").removeClass("none");
     }
+    $("canvas").removeClass("none");
     var canvas = document.getElementById("target");
     var dataURI = canvas.toDataURL();
     var image = document.getElementById("output");
     image.src = dataURI;
+    $("canvas").addClass("none");
+  });
+
+  $(".top-btn").click(function() {
+    $("html, body").animate({"scrollTop":0}, "100");
   });
 
 });
