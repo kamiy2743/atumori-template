4cf03b3b2f1b9275ca541970800ffb6acec05090
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2020-08-11 21:20:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,288 @@
+var info = [
+  {name:"電柱",code:1,mile:1000,img:"FtrTelephonepole_Remake_0_0",option:"広告なし"},
+  {name:"電柱",code:1,mile:1000,img:"FtrTelephonepole_Remake_1_0",option:"広告あり"},
+  {name:"街灯",code:2,mile:1000,img:"FtrStreetlamp_Remake_0_0",option:""},
+  {name:"街灯",code:2,mile:1000,img:"FtrStreetlamp_Remake_1_0",option:""},
+  {name:"街灯",code:2,mile:1000,img:"FtrStreetlamp_Remake_2_0",option:""},
+  {name:"街灯",code:2,mile:1000,img:"FtrStreetlamp_Remake_3_0",option:""},
+  {name:"仮設トイレ",code:3,mile:1000,img:"FtrWCPortable_Remake_0_0",option:""},
+  {name:"仮設トイレ",code:3,mile:1000,img:"FtrWCPortable_Remake_1_0",option:""},
+  {name:"仮設トイレ",code:3,mile:1000,img:"FtrWCPortable_Remake_2_0",option:""},
+  {name:"仮設トイレ",code:3,mile:1000,img:"FtrWCPortable_Remake_3_0",option:""},
+  {name:"仮設トイレ",code:3,mile:1000,img:"FtrWCPortable_Remake_4_0",option:""},
+  {name:"工事看板",code:4,mile:1000,img:"FtrSignConstruction_Remake_3_0",option:""},
+  {name:"工事看板",code:4,mile:1000,img:"FtrSignConstruction_Remake_4_0",option:""},
+  {name:"工事看板",code:4,mile:1000,img:"FtrSignConstruction_Remake_0_0",option:""},
+  {name:"工事看板",code:4,mile:1000,img:"FtrSignConstruction_Remake_1_0",option:"リセット"},
+  {name:"工事看板",code:4,mile:1000,img:"FtrSignConstruction_Remake_2_0",option:"ラケット"},
+  {name:"ビーチの監視台",code:5,mile:1000,img:"FtrMonitoringchair_Remake_0_0",option:""},
+  {name:"ビーチの監視台",code:5,mile:1000,img:"FtrMonitoringchair_Remake_1_0",option:""},
+  {name:"ビーチの監視台",code:5,mile:1000,img:"FtrMonitoringchair_Remake_2_0",option:""},
+  {name:"ビーチの監視台",code:5,mile:1000,img:"FtrMonitoringchair_Remake_3_0",option:""},
+  {name:"ビーチの監視台",code:5,mile:1000,img:"FtrMonitoringchair_Remake_4_0",option:""},
+  {name:"お菓子の自販機",code:1,mile:2000,img:"FtrFoodmachine_Remake_0_0",option:""},
+  {name:"お菓子の自販機",code:1,mile:2000,img:"FtrFoodmachine_Remake_1_0",option:""},
+  {name:"お菓子の自販機",code:1,mile:2000,img:"FtrFoodmachine_Remake_2_0",option:""},
+  {name:"お菓子の自販機",code:1,mile:2000,img:"FtrFoodmachine_Remake_3_0",option:""},
+  {name:"観光望遠鏡",code:2,mile:2000,img:"FtrTelescopeSightseeing_Remake_0_0",option:""},
+  {name:"観光望遠鏡",code:2,mile:2000,img:"FtrTelescopeSightseeing_Remake_1_0",option:""},
+  {name:"観光望遠鏡",code:2,mile:2000,img:"FtrTelescopeSightseeing_Remake_2_0",option:""},
+  {name:"観光望遠鏡",code:2,mile:2000,img:"FtrTelescopeSightseeing_Remake_3_0",option:""},
+  {name:"観光望遠鏡",code:2,mile:2000,img:"FtrTelescopeSightseeing_Remake_4_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_0_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_1_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_2_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_3_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_4_0",option:""},
+  {name:"自販機",code:3,mile:2000,img:"FtrDrinkmachine_Remake_5_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_0_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_1_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_2_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_3_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_4_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_5_0",option:""},
+  {name:"スプリング遊具",code:4,mile:2000,img:"FtrSpringrider_Remake_6_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_0_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_1_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_2_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_3_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_4_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_5_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_6_0",option:""},
+  {name:"パブリックベンチ",code:5,mile:2000,img:"FtrPublicbench_Remake_7_0",option:""},
+  {name:"公園の時計",code:1,mile:2400,img:"FtrParkclock_Remake_0_0",option:""},
+  {name:"公園の時計",code:1,mile:2400,img:"FtrParkclock_Remake_1_0",option:""},
+  {name:"公園の時計",code:1,mile:2400,img:"FtrParkclock_Remake_2_0",option:""},
+  {name:"公園の時計",code:1,mile:2400,img:"FtrParkclock_Remake_3_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_0_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_1_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_2_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_3_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_4_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_5_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_6_0",option:""},
+  {name:"電話ボックス",code:2,mile:2400,img:"FtrTelephonebox_Remake_7_0",option:""},
+  {name:"わたあめの屋台",code:1,mile:3000,img:"FtrCottoncandymachine_Remake_0_0",option:""},
+  {name:"わたあめの屋台",code:1,mile:3000,img:"FtrCottoncandymachine_Remake_1_0",option:""},
+  {name:"わたあめの屋台",code:1,mile:3000,img:"FtrCottoncandymachine_Remake_2_0",option:""},
+  {name:"ジャングルジム",code:2,mile:3000,img:"FtrJunglegym_Remake_0_0",option:""},
+  {name:"ジャングルジム",code:2,mile:3000,img:"FtrJunglegym_Remake_1_0",option:""},
+  {name:"ジャングルジム",code:2,mile:3000,img:"FtrJunglegym_Remake_2_0",option:""},
+  {name:"ジャングルジム",code:2,mile:3000,img:"FtrJunglegym_Remake_3_0",option:""},
+  {name:"ジャングルジム",code:2,mile:3000,img:"FtrJunglegym_Remake_4_0",option:""},
+  {name:"ソーラーパネル",code:1,mile:4000,img:"FtrSolarpanel_Remake_0_0",option:""},
+  {name:"ソーラーパネル",code:1,mile:4000,img:"FtrSolarpanel_Remake_1_0",option:""},
+  {name:"風力発電",code:2,mile:4000,img:"FtrWindturbine_Remake_0_0",option:"ブルー"},
+  {name:"風力発電",code:2,mile:4000,img:"FtrWindturbine_Remake_1_0",option:"レッド"},
+  {name:"パラボラアンテナ",code:3,mile:4000,img:"FtrParabolicantenna_Remake_0_0",option:""},
+  {name:"パラボラアンテナ",code:3,mile:4000,img:"FtrParabolicantenna_Remake_1_0",option:""},
+  {name:"パラボラアンテナ",code:3,mile:4000,img:"FtrParabolicantenna_Remake_2_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_0_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_1_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_2_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_3_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_4_0",option:""},
+  {name:"灯台",code:1,mile:5000,img:"FtrLighthouse_Remake_5_0",option:""},
+  {name:"怪獣",code:2,mile:5000,img:"FtrMonster_Remake_0_0",option:""},
+  {name:"怪獣",code:2,mile:5000,img:"FtrMonster_Remake_1_0",option:""},
+  {name:"怪獣",code:2,mile:5000,img:"FtrMonster_Remake_2_0",option:""},
+  {name:"怪獣",code:2,mile:5000,img:"FtrMonster_Remake_3_0",option:""},
+  {name:"プール",code:3,mile:5000,img:"FtrPool_Remake_0_0",option:""},
+  {name:"プール",code:3,mile:5000,img:"FtrPool_Remake_1_0",option:""},
+  {name:"プール",code:3,mile:5000,img:"FtrPool_Remake_2_0",option:""},
+  {name:"まわる<br>コーヒーカップ",code:4,mile:5000,img:"FtrTurncup_Remake_0_0",option:""},
+  {name:"まわる<br>コーヒーカップ",code:4,mile:5000,img:"FtrTurncup_Remake_1_0",option:""},
+  {name:"まわる<br>コーヒーカップ",code:4,mile:5000,img:"FtrTurncup_Remake_2_0",option:""},
+  {name:"まわる<br>コーヒーカップ",code:4,mile:5000,img:"FtrTurncup_Remake_3_0",option:""}
+];
+info.forEach((item, i) => {
+  item.no = i;
+});
+var miles = [1000,2000,2400,3000,4000,5000];
+var autoData = [
+  [[24,86,2,1,54,74,72,94],[22,84,4,1,52,74,72,92],[23,85,5,0,53,73,71,93],[21,87,3,0,51,73,71,91]],
+  [[32,65,79,75,90],[34,81,64,77,89],[33,63,80,76,88],[35,65,82,75,90],[31,64,78,77,89],[30,63,83,76,88]],
+  [[7,13,18,25,68],[8,14,19,26,69],[9,15,20,27,70],[10,11,16,28,66],[6,12,17,29,67]],
+  [[57,46],[56,45],[55,44],[61,50],[58,47],[59,48],[60,49],[62,43]],
+  [[36],[38],[40],[39],[41],[42],[37]]
+];
+
+//データ読み込み　
+function dataLoad() {
+  if(localStorage.data) {
+    JSON.parse(localStorage.data).forEach((data,i) => {
+      $(`.item-main[no="${data.no}"]`).attr("code",data.code);
+    });
+  }
+}
+
+//生成
+function itemsAdd() {
+  miles.forEach((mile,f) => {
+    $(".items").append(`
+      <tr mile="${mile}" class="mile-title">
+        <td colspan="6"><span>${mile} マイル</span><div class="all-delete all-btn btn">全削除</div><div class="all-select all-btn btn">全選択</div></td>
+      </tr>
+    `);
+    var ob = info.filter((value) => value.mile == mile);
+    for(e=ob[ob.length-1].code;e>0;e--) {
+      var st = ob.filter((value) => value.code == e);
+      var items = `<tr><td class="item-title">${st[0].name}</td>`;
+      if(st.length <= 5) {
+        for(i=0;i<st.length;i++) {
+          items += `<td class="item"><div class="item-main" no="${st[i].no}" code="0" mile="${mile}"><img src="image/${st[i].img}.png"><p>${st[i].option}</p></div></td>`;
+        }
+        for(i=st.length;i<5;i++) {
+          items += `<td class="item"></td>`;
+        }
+      } else {
+        items = `<tr><td class="item-title" rowspan="2">${st[0].name}</td>`;
+        for(i=0;i<5;i++) {
+          items += `<td class="item"><div class="item-main" no="${st[i].no}" code="0" mile="${mile}"><img src="image/${st[i].img}.png"><p>${st[i].option}</p></div></td>`;
+          if(i == 4) {items+="</tr><tr>"}
+        }
+        for(i=5;i<st.length;i++) {
+          items += `<td class="item"><div class="item-main" no="${st[i].no}" code="0" mile="${mile}"><img src="image/${st[i].img}.png"><p>${st[i].option}</p></div></td>`;
+        }
+        for(i=st.length-5;i<5;i++) {
+          items += `<td class="item"></td>`;
+        }
+      }
+      $(`.items .mile-title[mile="${mile}"]`).after(items+="</tr>");
+    }
+
+    var ob = info.filter((value) => value.mile == mile);
+    $(".capture-main").append(`
+      <tr mile="${mile}" class="mile-title">
+        <td rowspan="${ob[ob.length-1].code+1}"><p>${mile}<br><span>マイル</span></p></td>
+      </tr>
+    `);
+    for(e=ob[ob.length-1].code;e>0;e--) {
+      var st = ob.filter((value) => value.code == e);
+      var items = `<tr><td class="item-title"><p>${st[0].name}</p></td>`;
+      for(i=0;i<st.length;i++) {
+        items += `<td class="item"><div class="item-main" no="${st[i].no}" code="0" mile="${mile}"><img src="image/${st[i].img}.png"><p>${st[i].option}</p></div></td>`;
+      }
+      for(i=st.length;i<8;i++) {
+        items += `<td class="item"></td>`;
+      }
+      $(`.capture-main .mile-title[mile="${mile}"]`).after(items+="</tr>");
+    }
+    if(f == 5) {
+      dataLoad();
+    }
+  });
+}
+itemsAdd();
+
+//ページリンク
+  $(".series-btn").click(function() {
+    location.href = "https://kamiy2743.github.io/series.template/";
+  });
+  $(".old-ver").click(function() {
+    location.href = "https://kamiy2743.github.io/old-template/";
+  });
+
+//自動入力
+  $(".auto-menu").click(function() {
+    $(".menu-icon .fas").toggle();
+    $(".menu-main").slideToggle();
+  });
+  //実行
+  $(".auto-do").click(function() {
+    var arr = [];
+    $(".item-main").attr("code",0);
+    $(".q-list input:checked").each(function() {
+      var val = $(this).attr("id").slice(-1);
+      arr.push(val);
+    });
+    if(arr.length == 5) {
+      $(".auto-succsess").show();
+      $(".auto-error").hide();
+      arr.forEach((color,i) => {
+        autoData[i][color-1].forEach((no) => {
+          $(`.item-main[no="${no}"]`).attr("code",1);
+        });
+      });
+      dataSave();
+    } else {
+      $(".auto-succsess").hide();
+      $(".auto-error").show();
+    }
+  });
+
+//マーク選択
+  $(".mark-btn").click(function() {
+    $(".mark-btn").removeClass("mark-selected");
+    $(this).addClass("mark-selected");
+  });
+
+//マーク
+  function dataSave() {
+    var gdata = [];
+    $('.items .item-main[code!="0"]').each(function(i) {
+      gdata.push({no:$(this).attr("no"),code:$(this).attr("code")});
+    });
+    localStorage.data = JSON.stringify(gdata);
+  }
+  $(".item-main").click(function() {
+    var no = $(this).attr("no");
+    var value = $(".mark-selected").attr("value");
+    $(this).attr("code")==0 ? code = value : code = 0;
+    $(`.item-main[no="${no}"]`).attr("code",code);
+    dataSave();
+  });
+  //全選択
+  $(".all-select").click(function() {
+    var value = $(".mark-selected").attr("value");
+    var mile = $(this).parents(".mile-title").attr("mile");
+    $(`.item-main[mile="${mile}"]`).attr("code",value);
+    dataSave();
+  });
+  //全削除
+  $(".all-delete").click(function() {
+    var mile = $(this).parents(".mile-title").attr("mile");
+    $(`.item-main[mile="${mile}"]`).attr("code",0);
+    dataSave();
+  });
+
+//変換ページ
+  $(".convert-btn").click(function() {
+    $("main").hide();
+    $(".convert-page").show();
+    convertImage();
+  });
+  $(".page-close").click(function() {
+    $("main").show();
+    $(".convert-page").hide();
+  });
+  //画像関数
+  function convertImage() {
+    $("html,body").scrollTop(0);
+    $("#target").remove();
+    $(".capture-img").hide();
+    $(".capture,.laoding").show();
+    setTimeout(function() {
+      html2canvas(document.querySelector(".capture")).then(canvas => {
+        $("main").append(canvas);
+        $("canvas").attr("id","target");
+        var canvast = document.getElementById("target");
+        var dataURI = canvast.toDataURL();
+        $(".capture-img").attr("src",dataURI).show();
+        $(".img-link").attr("href",dataURI);
+        $(".capture,.laoding").hide();
+        $("#target").remove();
+        $(".capture-img").show();
+        dataURI = '';
+      });
+    },100);
+  }
+  //説明非表示
+  $("#input").click(function() {
+    $(this).prop("checked") ? $(".mark-exp").hide() : $(".mark-exp").show();
+    convertImage();
+  });
+  //ダウンロードボタン
+  $(".download-btn").click(function() {
+    $(".img-link")[0].click();
+  });
