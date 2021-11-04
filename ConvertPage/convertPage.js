ff7cfee4794779b6311e950f0850211f915b129a
kamiy2743 <46000642+kamiy2743@users.noreply.github.com>
2021-11-05 01:13:08 +0900

    Add files via upload

--- ConvertPage/convertPage.js
+++ ConvertPage/convertPage.js
@@ -11,58 +11,104 @@ function start() {
     }
 
     // 画像に変換
-    convertToImage()
+    convertToImage();
 }
 
 // 画像変換用テーブル生成
 function generateItemElements() {
     // 追加する要素を文字列にしておいて後で一括追加
-    var rawElement = "";
+    var rawElement1 = "";
+    var rawElement2 = "";
     var index = 0;
+    var bgcolor = -1;
     
     ITEM_DATAS.forEach(item_datas => {
         // マイル表示部分
         var mile = item_datas.mile;
-        rawElement += `
-            <tr>
-                <td class="mile-title" rowspan="${item_datas.items.length + 1}"><p>${mile}<br><span>マイル</span></p></td>
-            </tr>
-        `;
-
-        item_datas.items.forEach(items => {
-            // アイテム名表示部分
-            var item_name = items.name;
-            rawElement += `<tr><td class="item-name">${item_name}</td>`;
-
-            // 8個追加
-            for (var i = 0; i < 8; i++) {
-                // アイテムの個数が8個未満なら空の要素で埋める
-                if (i < items.attrs.length) {
-                    // アイテムの画像と補足部分
-                    var attrs = items.attrs[i];
-                    var img = attrs.img;
-                    var info_text = attrs.info;
-                    rawElement += `
-                        <td class="item-container">
-                            <div class="item-main" index="${index++}" state="0">
-                                <img class="item-image" src="../image/${img}.png">
-                                <p class="info-text">${info_text}</p>
-                            </div>
-                        </td>
-                    `;
-                } else {
-                    rawElement += `
-                        <td class="item-container">
-                            <div class="item-main item-empty" state="0">
-                            </div>
-                        </td>
-                    `;
+        if (mile <= 2400) {
+            rawElement1 += `
+                <tr>
+                    <td class="mile-title" rowspan="${item_datas.items.length + 1}"><p>${mile}<br><span>マイル</span></p></td>
+                </tr>
+            `;
+    
+            item_datas.items.forEach((items, i) => {
+                // アイテム名表示部分
+                var item_name = items.name;
+                var td_bottom = i == item_datas.items.length-1 ? "td-bottom" : "";
+                rawElement1 += `<tr><td class="item-name ${td_bottom}">${item_name}</td>`;
+    
+                // 8個追加
+                for (var i = 0; i < 8; i++) {
+                    // アイテムの個数が8個未満なら空の要素で埋める
+                    if (i < items.attrs.length) {
+                        // アイテムの画像と補足部分
+                        var attrs = items.attrs[i];
+                        var img = attrs.img;
+                        var info_text = attrs.info;
+                        rawElement1 += `
+                            <td class="item-container ${td_bottom}">
+                                <div class="item-main" index="${index++}" state="0">
+                                    <img class="item-image" src="../image/${img}.png">
+                                    <p class="info-text">${info_text}</p>
+                                </div>
+                            </td>
+                        `;
+                    } else {
+                        rawElement1 += `
+                            <td class="item-container ${td_bottom}">
+                                <div class="item-main item-empty" state="0">
+                                </div>
+                            </td>
+                        `;
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            rawElement2 += `
+                <tr>
+                    <td class="mile-title" rowspan="${item_datas.items.length + 1}"><p>${mile}<br><span>マイル</span></p></td>
+                </tr>
+            `;
+    
+            item_datas.items.forEach((items, i) => {
+                // アイテム名表示部分
+                var item_name = items.name;
+                var td_bottom = i == item_datas.items.length-1 ? "td-bottom" : "";
+                rawElement2 += `<tr><td class="item-name ${td_bottom}">${item_name}</td>`;
+    
+                // 8個追加
+                for (var i = 0; i < 8; i++) {
+                    // アイテムの個数が8個未満なら空の要素で埋める
+                    if (i < items.attrs.length) {
+                        // アイテムの画像と補足部分
+                        var attrs = items.attrs[i];
+                        var img = attrs.img;
+                        var info_text = attrs.info;
+                        rawElement2 += `
+                            <td class="item-container ${td_bottom}">
+                                <div class="item-main" index="${index++}" state="0">
+                                    <img class="item-image" src="../image/${img}.png">
+                                    <p class="info-text">${info_text}</p>
+                                </div>
+                            </td>
+                        `;
+                    } else {
+                        rawElement2 += `
+                            <td class="item-container ${td_bottom}">
+                                <div class="item-main item-empty" state="0">
+                                </div>
+                            </td>
+                        `;
+                    }
+                }
+            });
+        }
+        
     });
     // 一括追加
-    $(".capture-table").append(rawElement);
+    $(".capture-table[imageIndex='0']").append(rawElement1);
+    $(".capture-table[imageIndex='1']").append(rawElement2);
 }
 
 // ローカルストレージから読み込み
@@ -74,15 +120,24 @@ function loadLocalData() {
 
 // 画像に変換
 function convertToImage() {
-    $("#capture").removeClass("none");
-    html2canvas(document.querySelector("#capture")).then(canvas => { 
+    // 1枚目
+    $("#capture1").removeClass("none");
+    html2canvas(document.querySelector("#capture1")).then(canvas => { 
         let imageURL = canvas.toDataURL();
-        $(".captured-image").attr("src", imageURL);
-        $(".captured-image-link").attr("href", imageURL);
-        $(".waiting-text").addClass("none");
+        $(".captured-image[imageIndex='0']").attr("src", imageURL);
+        $(".captured-image-link[imageIndex='0']").attr("href", imageURL);
     });
-    $("#capture").addClass("none");
-    $(".captured-image").removeClass("none");
+    $("#capture1").addClass("none");
+    // 2枚目
+    $("#capture2").removeClass("none");
+    html2canvas(document.querySelector("#capture2")).then(canvas => { 
+        let imageURL = canvas.toDataURL();
+        $(".captured-image[imageIndex='1']").attr("src", imageURL);
+        $(".captured-image-link[imageIndex='1']").attr("href", imageURL);
+        $(".waiting-text").addClass("none");
+        $(".download-btn-container").removeClass("none")
+    });
+    $("#capture2").addClass("none");
 }
 
 // ダウンロードボタン
